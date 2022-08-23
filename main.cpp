0c1b1633c722fb7638663fd3bee018bcd4a6268f
April <april.l.petersen@protonmail.com>
2022-08-23 15:10:31 -0500

    add basic windows

--- main.cpp
+++ main.cpp
@@ -12,6 +12,13 @@
 int windowWidth = 1280,
     windowHeight = 720;
 
+static bool show_controls = true;
+static bool show_settings = false;
+
+void showControls(bool *p_open, SDL_Window *window, float menubar_height);
+void showViewer(SDL_Window *window, float &menubar_height);
+void showSettings(bool *p_open, SDL_Window *window);
+
 int main()
 {
 
@@ -62,7 +69,7 @@ int main()
             | SDL_WINDOW_ALLOW_HIGHDPI
     );
     SDL_Window *window = SDL_CreateWindow(
-            "Dear ImGui SDL",
+            "slimulation",
             SDL_WINDOWPOS_CENTERED,
             SDL_WINDOWPOS_CENTERED,
             windowWidth,
@@ -164,44 +171,39 @@ int main()
         ImGui_ImplSDL2_NewFrame(window);
         ImGui::NewFrame();
 
-        // a window is defined by Begin/End pair
+        float menubar_height{ 0 };
+
+        if (ImGui::BeginMainMenuBar())
         {
-            static int counter = 0;
-            // get the window size as a base for calculating widgets geometry
-            int sdl_width = 0, sdl_height = 0, controls_width = 0;
-            SDL_GetWindowSize(window, &sdl_width, &sdl_height);
-            controls_width = sdl_width;
-            // make controls widget width to be 1/3 of the main window width
-            if ((controls_width /= 3) < 300) { controls_width = 300; }
-
-            // position the controls widget in the top-right corner with some margin
-            ImGui::SetNextWindowPos(ImVec2(10, 10), ImGuiCond_Always);
-            // here we set the calculated width and also make the height to be
-            // be the height of the main window also with some margin
-            ImGui::SetNextWindowSize(
-                    ImVec2(static_cast<float>(controls_width), static_cast<float>(sdl_height - 20)),
-                    ImGuiCond_Always
-            );
-            // create a window and append into it
-            ImGui::Begin("Controls", NULL, ImGuiWindowFlags_NoResize);
-
-            ImGui::Dummy(ImVec2(0.0f, 1.0f));
-            ImGui::TextColored(ImVec4(1.0f, 0.0f, 1.0f, 1.0f), "Platform");
-            ImGui::Text("%s", SDL_GetPlatform());
-            ImGui::Text("CPU cores: %d", SDL_GetCPUCount());
-            ImGui::Text("RAM: %.2f GB", SDL_GetSystemRAM() / 1024.0f);
-
-            // buttons and most other widgets return true when clicked/edited/activated
-            if (ImGui::Button("Counter button"))
+            if (ImGui::BeginMenu("File"))
             {
-                std::cout << "counter button clicked\n";
-                counter++;
+                if (ImGui::MenuItem("Test")) {}
+                ImGui::EndMenu();
             }
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::End();
+            if (ImGui::BeginMenu("Edit"))
+            {
+                if (ImGui::MenuItem("Undo", "CTRL+Z")) {}
+                if (ImGui::MenuItem("Redo", "CTRL+Y", false, false)) {}  // Disabled item
+                ImGui::Separator();
+                if (ImGui::MenuItem("Settings", "")) { show_settings = true; }
+                ImGui::EndMenu();
+            }
+            if (ImGui::BeginMenu("View"))
+            {
+                if (ImGui::MenuItem("Controls", "", show_controls)) { show_controls = !show_controls; }
+                ImGui::EndMenu();
+            }
+            menubar_height = ImGui::GetWindowSize().y;
+            ImGui::EndMainMenuBar();
         }
+
+        showViewer(window, menubar_height);
+
+        if (show_controls)
+            showControls(&show_controls, window, menubar_height);
+
+        if (show_settings)
+            showSettings(&show_settings, window);
 
         // rendering
         ImGui::Render();
@@ -219,4 +221,73 @@ int main()
     SDL_Quit();
 
     return 0;
+}
+
+void showControls(bool *p_open, SDL_Window *window, float menubar_height)
+{
+    // get the window size as a base for calculating widgets geometry
+    int sdl_width = 0, sdl_height = 0, controls_width = 0;
+    SDL_GetWindowSize(window, &sdl_width, &sdl_height);
+    controls_width = sdl_width;
+    // make controls widget width to be 1/4 of the main window width
+    controls_width /= 4;
+
+    // position the controls widget in the top-right corner with some margin
+    ImGui::SetNextWindowPos(ImVec2(0, menubar_height), ImGuiCond_Always);
+    // here we set the calculated width and also make the height to be
+    // the height of the main window also with some margin
+    ImGui::SetNextWindowSize(
+            ImVec2(static_cast<float>(controls_width), static_cast<float>(sdl_height - menubar_height)),
+            ImGuiCond_Always
+    );
+    // create a window and append into it
+    ImGui::Begin("Controls", p_open, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
+
+    ImGui::Dummy(ImVec2(0.0f, 1.0f));
+    ImGui::TextColored(ImVec4(1.0f, 0.0f, 1.0f, 1.0f), "Platform");
+    ImGui::Text("%s", SDL_GetPlatform());
+    ImGui::Text("CPU cores: %d", SDL_GetCPUCount());
+    ImGui::Text("RAM: %.2f GB", SDL_GetSystemRAM() / 1024.0f);
+
+    // buttons and most other widgets return true when clicked/edited/activated
+    if (ImGui::Button("Start"))
+    {
+        std::cout << "Start button clicked" << std::endl;
+    }
+
+    ImGui::End();
+}
+
+void showViewer(SDL_Window *window, float &menubar_height)
+{
+    // get the window size as a base for calculating widgets geometry
+    int sdl_width = 0, sdl_height = 0, viewer_width = 0;
+    SDL_GetWindowSize(window, &sdl_width, &sdl_height);
+    viewer_width = sdl_width;
+    // make viewer widget width to be 3/4 of the main window width
+    if (show_controls)
+        viewer_width *= 0.75;
+
+    // position the controls widget in the top-left corner with some margin
+    ImGui::SetNextWindowPos(ImVec2(sdl_width - viewer_width, menubar_height), ImGuiCond_Always);
+    // here we set the calculated width and also make the height to be
+    // the height of the main window also with some margin
+    ImGui::SetNextWindowSize(
+            ImVec2(static_cast<float>(viewer_width), static_cast<float>(sdl_height - menubar_height)),
+            ImGuiCond_Always
+    );
+    // create a window and append into it
+    ImGui::Begin("Viewer", NULL, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoDecoration);
+
+    ImGui::Text("Slime simulation through OpenGL goes here");
+
+    ImGui::End();
+}
+
+void showSettings(bool *p_open, SDL_Window *window)
+{
+    ImGui::Begin("Settings", p_open);
+    int n = 0;
+    ImGui::Combo("Font Size", &n, "12\0 14\0\0");
+    ImGui::End();
 }
